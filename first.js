ff06249a16e7ee386d1dceb9720a95b045c02353
unknown <saifniaz04@gmail.com>
2020-10-04 11:50:58 +0600

    added a innner function

--- first.js
+++ first.js
@@ -1,6 +1,11 @@
 function print() {
 
     console.log("i am learning git tutorial");
+    function say() {
+
+        console.log("Now i have learned to work with online repo");
+    }
+    say();
     
 }
 print();
